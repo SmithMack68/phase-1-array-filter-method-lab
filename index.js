18d456a11ca18baa1f6b7c6ef44c436f3be3ffdb
Candice Smith MacKenzie <Mahony8@aol.com>
2021-12-03 21:33:08 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function findMatching(array, string) {
+    const driver = array.filter(drivers => drivers.toUpperCase() === string.toUpperCase())
+    return driver;
+}
+function fuzzyMatch(array, string) {
+    const driver = array.filter(drivers => drivers.startsWith(string))
+    return driver;
+}
+function matchName(array,string) {
+    const driver = array.filter(drivers => drivers.name === string)
+    return driver;
+}
 // Code your solution here
